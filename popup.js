d0ccfbf8aab2beb64c5f27669f15ca2693e85786
AleksandrTheGreat21 <100033042@mvla.net>
2024-06-13 11:11:10 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,9 +1,2 @@
 console.log('This is a popup!');
-//playsound("camera-shutter-199580.mp3",false)
-function playound() {
-  const audio = document.getElementById('myAudio');
-  audio.play();
-}
-function alsoPlaySound() {
-  playSound("camera-shutter-199580.mp3",false);
-}
+
